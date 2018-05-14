a127edb8c454a2b97e1d7b92325b98fcb26eb2ca
Ali <s4lish@gmail.com>
2018-05-14 14:21:14 +0430

    new

---
+++ test2/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  test2
+//
+//  Created by Ali Seyedi on 4/25/18.
+//  Copyright © 2018 Arsham co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var ali="ali"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
